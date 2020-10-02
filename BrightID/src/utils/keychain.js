add1c20c8d614a6e6df0aca8ead2594a146d7f8d
rnbwd <dwisner6@gmail.com>
2020-10-02 17:00:53 +0800

    use react-native-sensitive-storage

--- BrightID/src/utils/keychain.js
+++ BrightID/src/utils/keychain.js
@@ -1,41 +1,83 @@
 // @flow
-import {
-  getGenericPassword,
-  setGenericPassword,
-  STORAGE_TYPE,
-} from 'react-native-keychain';
+import { getGenericPassword } from 'react-native-keychain';
 import store from '@/store';
+import SInfo from 'react-native-sensitive-info';
 import { objToB64, b64ToUint8Array } from './encoding';
 import { DEVICE_ANDROID } from './constants';
 
+// setup preferences from react-native-sensitive-info
+if (DEVICE_ANDROID) {
+  SInfo.setInvalidatedByBiometricEnrollment(false);
+}
+
+const sharedPreferencesName = 'brightid_shared_preferences';
+const keychainService = 'org.brightid.demo.SharedKeychain';
+
+const uInt8SecretKey = (key) => {
+  let secretKey = b64ToUint8Array(key);
+  if (secretKey.length < 10) {
+    throw new Error('secret key does not exist');
+  }
+  return secretKey;
+};
+
 export const obtainKeys = async () => {
   try {
+    let id = await SInfo.getItem('brightid_id', {
+      sharedPreferencesName,
+      keychainService,
+    });
+
+    let b64SecretKey = await SInfo.getItem('brightid_secretkey', {
+      sharedPreferencesName,
+      keychainService,
+    });
+
+    // if stored in sensitive info, return immediately
+    if (!id || !b64SecretKey) {
+      id = store.getState().user.id;
+      b64SecretKey = store.getState().user;
+
+      if (id && b64SecretKey) {
+        await saveSecretKey(id, b64SecretKey);
+      } else {
+        throw new Error('retrieve key from react-native-keychain');
+      }
+    }
+
+    let secretKey = uInt8SecretKey(b64SecretKey);
+
+    return { username: id, secretKey };
+    // first check if secretKey is located in redux
+  } catch (err) {
     let genericPassword = await getGenericPassword();
     if (!genericPassword) {
       throw new Error('secret key does not exist');
     }
     let { username, password } = genericPassword;
-    let secretKey = b64ToUint8Array(password);
-    if (secretKey.length < 10) {
-      throw new Error('secret key does not exist');
-    }
+
+    let secretKey = uInt8SecretKey(password);
+    await saveSecretKey(username, secretKey);
 
     return { username, secretKey };
-  } catch (err) {
-    console.log(err.message);
-    let { id, secretKey } = store.getState().user;
-    return { username: id, secretKey: b64ToUint8Array(secretKey) };
   }
 };
 
-export const saveSecretKey = async (id: string, secretKey: string) => {
+export const saveSecretKey = async (id: string, secretKey: UInt8Array) => {
   try {
-    if (DEVICE_ANDROID) {
-      let opts = { storage: STORAGE_TYPE.AES };
-      await setGenericPassword(id, objToB64(secretKey), opts);
-    } else {
-      await setGenericPassword(id, objToB64(secretKey));
+    await SInfo.setItem('brightid_id', id, {
+      sharedPreferencesName,
+      keychainService,
+    });
+
+    if (typeof secretKey !== 'string') {
+      secretKey = objToB64(secretKey);
     }
+
+    await SInfo.setItem('brightid_secretkey', secretKey, {
+      sharedPreferencesName,
+      keychainService,
+    });
   } catch (err) {
     console.error(err.message);
   }
